81157939b1116234a1a16820ea95b922543490e8
Mário César Silva <mcgato33@hotmail.com>
2020-08-04 14:45:46 -0100

    configuracao inicial

---
+++ backend/README.md
@@ -0,0 +1,12 @@
+# Funcionalidades
+
+## Conexões
+
+- Rota para listar o tatal de conexões realizadas;
+- Rota para criar uma nova conexão;
+
+## Auloas
+
+- Rota criar uma aula;
+- Rota para listar aulas;
+  - Filtrar por matéria, dia da semana e horário       desponivel pelo professor
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, respose) => {
-  return respose.send("oiii");
+  return respose.json({ message: "Oi mario"});
 })
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log("Servidor inicializado, porta 3333");
+});
