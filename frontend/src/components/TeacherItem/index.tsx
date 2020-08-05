534cffb719f3370fc1286b51d7eb39aab03e300d
Mário César Silva <mcgato33@hotmail.com>
2020-08-05 14:21:11 -0100

    finalizando

--- frontend/src/components/TeacherItem/index.tsx
+++ frontend/src/components/TeacherItem/index.tsx
@@ -2,36 +2,58 @@ import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-function TeachertItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+export interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
-          <header>
-            <img src="https://avatars3.githubusercontent.com/u/24758286?s=460&u=d3b6fcc00916078ae870fd88b802b006b7c3ac72&v=4" alt="Mário César"/>
-            <div>
-              <strong>Mário César Silva</strong>
-              <span>Quimica</span>
-            </div>
-          </header>
-          <p>Entusiasta das melhores tecnologias de química avançada. 
-            <br/><br/>
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. 
-            Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-          </p>
+      <header>
+        <img src={teacher.avatar} alt={teacher.name} />
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
+      <p>{teacher.bio}</p>
 
-          <footer>
-            <p>
-              Preco/hora
-              <strong>500$00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="whatsapp"/>
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ {teacher.cost}</strong>
+        </p>
+        <a
+          onClick={createNewConnection}
+          target="_blank"
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
+          <img src={whatsappIcon} alt="Whatsapp"/>
+          Entrar em contato
+        </a>
+      </footer>
+    </article>
   );
 }
 
-export default TeachertItem;
+export default TeacherItem;
